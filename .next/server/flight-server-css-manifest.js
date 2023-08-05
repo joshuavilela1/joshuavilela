fc9757dcf6d8adce2e450ab8a8ec5a2511b65628
joshuavilela <joshuavilela1@gmail.com>
2023-08-04 18:51:13 -0700

    Created Development Branch and updated Git Ignore File

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/joshuavilela/portfolio_project/app/layout.tsx": [
+      "/home/joshuavilela/portfolio_project/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/joshuavilela/portfolio_project/styles/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/joshuavilela/portfolio_project/app/page": [
+      "/home/joshuavilela/portfolio_project/styles/globals.css",
+      "/home/joshuavilela/portfolio_project/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
