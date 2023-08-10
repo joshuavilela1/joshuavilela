3d986d4c192907b7858605ad530b439d1834ae34
joshuavilela <joshuavilela1@gmail.com>
2023-08-10 01:21:43 -0700

    Example of tailwind css using custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className="font-montserrat">Hello Worlds</div>;
+  return <div className="font-montserrat">Hello World</div>;
 }
